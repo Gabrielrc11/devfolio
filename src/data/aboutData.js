67b0e9e23ea7dd48c34ec059e0d611478310b4eb
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-04-10 12:25:51 -0300

    refact: Reestruturação dos dados e página About

---
+++ src/data/aboutData.js
@@ -0,0 +1,99 @@
+export const skills = [
+  {
+    name: 'JavaScript',
+    icon: 'devicon-javascript-plain colored',
+    color: '#F7DF1E'
+  },
+  {
+    name: 'PHP',
+    icon: 'devicon-php-plain colored',
+    color: '#777BB4'
+  },
+  {
+    name: 'Python',
+    icon: 'devicon-python-plain colored',
+    color: '#306998'
+  },
+  {
+    name: 'React',
+    icon: 'devicon-react-original colored',
+    color: '#61DAFB'
+  },
+  {
+    name: 'Laravel',
+    icon: 'devicon-laravel-plain colored',
+    color: '#FF2D20'
+  },
+  {
+    name: 'Django',
+    icon: 'devicon-django-plain colored',
+    color: '#092E20'
+  },
+  {
+    name: 'PostgreSQL',
+    icon: 'devicon-postgresql-plain colored',
+    color: '#336791'
+  },
+  {
+    name: 'MySQL',
+    icon: 'devicon-mysql-plain colored',
+    color: '#4479A1'
+  },
+  {
+    name: 'Docker',
+    icon: 'devicon-docker-plain colored',
+    color: '#2496ED'
+  },
+  {
+    name: 'Git',
+    icon: 'devicon-git-plain colored',
+    color: '#F05032'
+  },
+  {
+    name: 'Linux',
+    icon: 'devicon-linux-plain colored',
+    color: '#FCC624'
+  }
+];
+
+export const experiences = [
+  {
+    company: 'ChatADV',
+    position: 'Desenvolvedor Full Stack', 
+    period: 'Janeiro 2025 - Abril 2025',
+    description: 'Desenvolvimento e manutenção do sistema Web ChatADV, um sistema de IA para área jurídica brasileira.',
+    technologies: ['JavaScript', 'PHP', 'Python', 'React', 'Laravel', 'MySql', 'IA', 'Docker', 'Git']
+  },
+  {
+    company: 'MW Soluções',
+    position: 'Desenvolvedor de Analise de Dados',
+    period: 'Setembro 2024 - Fevereiro 2025',
+    description: 'Desenvolvimento e manutenção do EasyBI, um sistema de monitoramento voltado para análise e gestão de dados e metricas financeiras de provedores de internet.',
+    technologies: ['Python', 'PostgreSQL', 'MySQL', 'QuestDB', 'Docker', 'Git', 'Linux']
+  },
+  {
+    company: 'UFRN',
+    position: 'Cientista de Dados',
+    period: 'Setembro 2022 - Agosto 2023',
+    description: 'Analise e processamento de sinais de ratos em experimentos de corrida em esteira',
+    technologies: ['MATLAB', 'Python', 'Git']
+  }
+];
+
+export const education = [
+  {
+    course: 'Bacharelado em Ciências e Tecnologia',
+    institution: 'Universidade Federal do Rio Grande do Norte',
+    period: 'Setembro 2024 - Presente'
+  },
+  {
+    course: 'Técnico em Informática para Internet',
+    institution: 'Instituto Metrópole Digital',
+    period: 'Março 2022 - Janeiro 2025'
+  },
+  {
+    course: 'Técnico em Informática',
+    institution: 'Escola Agricola de Jundiai | UFRN',
+    period: 'Março 2021 - Dezembro 2023'
+  }
+]; 
