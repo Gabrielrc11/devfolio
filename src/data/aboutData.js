d0c1ebb3e721d9549f252c79d05a7f8fec819565
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-06-19 17:45:59 -0300

    feat: Refresh data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -19,15 +19,15 @@ export const skills = [
     icon: 'devicon-react-original colored',
     color: '#61DAFB'
   },
+    {
+    name: 'Vue.js',
+    icon: 'devicon-vuejs-plain colored',
+    color: '#61DAFB'
+  },
   {
     name: 'Laravel',
     icon: 'devicon-laravel-plain colored',
     color: '#FF2D20'
-  },
-  {
-    name: 'Django',
-    icon: 'devicon-django-plain colored',
-    color: '#092E20'
   },
   {
     name: 'PostgreSQL',
@@ -57,25 +57,32 @@ export const skills = [
 ];
 
 export const experiences = [
+    {
+    company: 'EasyRev',
+    position: 'Desenvolvedor Full Stack', 
+    period: 'Junho 2025 - Presente',
+    description: 'Atuo no desenvolvimento do sistema Web da EasyRev, uma plataforma para gerenciamento inteligente de hoteis. Minhas responsabilidades incluem a implementacão de novas funcionalidades, correção de bugs,otimizacão de desempenho e aprimoramento da experiência do usuário. Trabalho com tecnologias modernas como Laravel, Vue.js e Docker.',
+    technologies: ['JavaScript', 'PHP', 'Vue.js', 'Laravel','TailwindCss', 'MySQL', 'Docker', 'Git']
+  },
   {
     company: 'ChatADV',
     position: 'Desenvolvedor Full Stack', 
     period: 'Janeiro 2025 - Abril 2025',
-    description: 'Desenvolvimento e manutenção do sistema Web ChatADV, um sistema de IA para área jurídica brasileira.',
-    technologies: ['JavaScript', 'PHP', 'Python', 'React', 'Laravel', 'MySql', 'IA', 'Docker', 'Git']
+    description: 'Atuei como Pessoa Jurídica no desenvolvimento do sistema Web do ChatADV, uma plataforma de inteligência artificial voltada para o setor jurídico brasileiro. Minhas responsabilidades incluiram a implementação de novas funcionalidades, correção de bugs, otimização de desempenho e aprimoramento da experiência do usuário. Trabalhei com tecnologias modernas como React, Laravel e Docker, para garantir um sistema robusto, escalável e eficiente, contribuindo para a inovação e automação de processos jurídicos.',
+    technologies: ['JavaScript', 'PHP', 'Python', 'React', 'Laravel','MUI', 'MySQL', 'IA', 'Docker', 'Git']
   },
   {
     company: 'MW Soluções',
     position: 'Desenvolvedor de Analise de Dados',
     period: 'Setembro 2024 - Fevereiro 2025',
-    description: 'Desenvolvimento e manutenção do EasyBI, um sistema de monitoramento voltado para análise e gestão de dados e metricas financeiras de provedores de internet.',
+    description: 'Atuei na manutenção e customização do EasyBI, sistema de monitoramento voltado para análise e gestão de dados, utilizando ferramentas modernas como Python, SQL e Docker. Minhas responsabilidades incluem otimizar a interface do sistema, adaptar funcionalidades para atender demandas específicas dos clientes. Trabalho de forma colaborativa com a equipe e com os clientes para implementar melhorias e fornecer soluções personalizadas, alinhando tecnologia e estratégia para aprimorar a experiência de monitoramento e a precisão dos dados.',
     technologies: ['Python', 'PostgreSQL', 'MySQL', 'QuestDB', 'Docker', 'Git', 'Linux']
   },
   {
     company: 'UFRN',
     position: 'Cientista de Dados',
     period: 'Setembro 2022 - Agosto 2023',
-    description: 'Analise e processamento de sinais de ratos em experimentos de corrida em esteira',
+    description: 'Atuei como bolsista de pesquisa em ciência de dados, desenvolvendo redes neurais e realizando análise e visualização de dados utilizando programação com MATLAB. Durante esse período, trabalhei com dados cerebrais coletados de ratos em experimentos de corrida em esteira, aplicando técnicas de modelagem e interpretação de sinais para entender padrões e comportamentos. Minha atuação focou na análise precisa e no desenvolvimento de soluções baseadas em dados complexos, contribuindo para pesquisas inovadoras em neurociência.',
     technologies: ['MATLAB', 'Python', 'Git']
   }
 ];
